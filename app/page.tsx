ed66b3665fa7e0dec515df7815e711d6ca157690
Andrew <andrewsasamori@gmail.com>
2025-08-13 16:26:20 -0700

    Working website

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-6 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Andrew Sasamori
+      </h1>
+      <div className="mb-4 space-y-3">
+        <p className="text-gray-700 dark:text-gray-300 leading-relaxed">I'm passionate about sustainability in engineering and AI, healthcare systems, and electric vehicles. </p>
+        <p className="text-gray-700 dark:text-gray-300 leading-relaxed">Lately, I've been spending my time working out, reading, learning guitar, and improving my Japanese.</p>
+        
+        <p className="text-gray-700 dark:text-gray-300 leading-relaxed">
+          Connect with me on{' '}
+          <a href="https://www.linkedin.com/in/andrew-sasamori" target="_blank" rel="noopener noreferrer" className="underline text-sky-500">LinkedIn</a>,{' '}
+          <a href="https://github.com/asasamori" target="_blank" rel="noopener noreferrer" className="underline text-sky-500">GitHub</a>,{' '}
+          <a href="https://www.goodreads.com/user/show/141124879-andrew" target="_blank" rel="noopener noreferrer" className="underline text-sky-500">Goodreads</a>, or{' '}
+          <a href="https://www.strava.com/athletes/102980625" target="_blank" rel="noopener noreferrer" className="underline text-sky-500">Strava</a>!
+        </p>
+      </div>
+      {/* <div className="mt-6">
+        <BlogPosts />
+      </div> */}
+    </section>
+  )
+}
