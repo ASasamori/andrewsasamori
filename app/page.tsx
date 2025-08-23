3b4871d47eabaa411effc48120a436c11ddc439b
Andrew <andrewsasamori@gmail.com>
2025-08-22 17:15:39 -0700

    Changed main page profile view

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,47 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      {/* <h1 className="mb-6 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Andrew Sasamori
-      </h1> */}
-      <div className="mb-6 space-y-3">
-        <p className="text-gray-700 dark:text-gray-300 leading-relaxed">I'm passionate about sustainability in engineering and AI, healthcare systems, and electric vehicles. </p>
-        <p className="text-gray-700 dark:text-gray-300 leading-relaxed">Lately, I've been spending my time working out, reading, learning guitar, and improving my Japanese.</p>
-        
-        <p className="text-gray-700 dark:text-gray-300 leading-relaxed">
+      <div className="mb-6 space-y-6 text-gray-700 dark:text-gray-300 leading-relaxed">
+        <p>
+          I'm passionate about sustainability in engineering and AI, healthcare systems, and electric vehicles.
+        </p>
+
+        <p>
+          Lately, I've been spending my time working out, reading, learning guitar, and improving my Japanese.
+        </p>
+
+        <p>
           Connect with me on{' '}
-          <a href="https://www.linkedin.com/in/andrew-sasamori" target="_blank" rel="noopener noreferrer" className="underline text-sky-500">LinkedIn</a>,{' '}
-          <a href="https://github.com/asasamori" target="_blank" rel="noopener noreferrer" className="underline text-sky-500">GitHub</a>,{' '}
-          <a href="https://www.goodreads.com/user/show/141124879-andrew" target="_blank" rel="noopener noreferrer" className="underline text-sky-500">Goodreads</a>, or{' '}
-          <a href="https://www.strava.com/athletes/102980625" target="_blank" rel="noopener noreferrer" className="underline text-sky-500">Strava</a>!
+          <a
+            href="https://www.linkedin.com/in/andrew-sasamori"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-sky-500"
+          >
+            LinkedIn
+          </a>{' '}
+          or check out my{' '}
+          <a
+            href="https://github.com/asasamori"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-sky-500"
+          >
+            GitHub
+          </a>!
+        </p>
+
+        <p>
+          I'm very fortunate to be in the city of{' '}
+          <a
+            href="https://maps.app.goo.gl/m89L6P5mhwPuFgio8"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-sky-500"
+          >
+            San Francisco
+          </a>{', '}
+          and would love the chance to grab a coffee, play Ultimate frisbee together, or just chat!
         </p>
       </div>
 
