d8195b468c5b4dc68982b942642fe37e736d8a43
Andrew <andrewsasamori@gmail.com>
2025-10-14 10:58:36 -0700

    Added nice Yosemite pic

--- app/lib/random-image.ts
+++ app/lib/random-image.ts
@@ -1,5 +1,5 @@
 // Random image selection utility
-interface Person {
+interface Link {
   name: string
   url: string
 }
@@ -8,7 +8,7 @@ interface ImageData {
   src: string
   caption?: string
   caption2?: string
-  people?: Person[]
+  captionLink?: Link[]
 }
 
 export function getRandomImage(): ImageData {
@@ -17,7 +17,7 @@ export function getRandomImage(): ImageData {
       src: '/images/ts.jpeg',
       caption: '📷 by Albert',
       caption2: '📍 Tanah Lot, ID',
-      people:
+      captionLink:
       [
         {name: "Albert", url: 'https://www.linkedin.com/in/albertzhaoo/'},
         {name: "Tanah Lot, ID", url: 'https://maps.app.goo.gl/ySXcdh8WvFkjpamq5'}
@@ -27,7 +27,7 @@ export function getRandomImage(): ImageData {
       src: '/images/ts2.jpeg',
       caption: '📷 by Ben',
       caption2: '📍 Sete Cidades, Portugal',
-      people: 
+      captionLink: 
       [
         {name: "Ben", url: "https://www.linkedin.com/in/bennettetaylor/"},
         {name: 'Sete Cidades, Portugal', url: 'https://maps.app.goo.gl/BLqx8jmeH5dRA5mEA' },
@@ -37,7 +37,7 @@ export function getRandomImage(): ImageData {
       src: '/images/ts3.jpeg', 
       caption: 'With Michael, Ava, and Karston',
       caption2: '📍 Mt Tamalpais, CA',
-      people: [
+      captionLink: [
         { name: 'Michael', url: 'https://www.linkedin.com/in/michaelg-fong/' },
         { name: 'Karston', url: 'https://www.linkedin.com/in/karstonrunge/' },
         { name: 'Mt Tamalpais, CA', url: 'https://maps.app.goo.gl/HpsYjhVv43y7TS2J7'}
@@ -47,13 +47,23 @@ export function getRandomImage(): ImageData {
       src: '/images/ts4.jpeg',
       caption: 'With Arjun P., Gaurav, Tony, Arjun S., and Avi',
       caption2: 'Faithful to the Bay!',
-      people: [
+      captionLink: [
         {name: 'Arjun P.', url:'https://www.linkedin.com/in/arjun-patrawala/'},
         {name: 'Gaurav', url:'https://www.linkedin.com/in/gaurav-chakravarty/'},
         {name: 'Tony', url:'https://www.linkedin.com/in/tony-xin/'},
         {name: 'Arjun S.', url:'https://www.linkedin.com/in//arjunsk18/'},
         {name: 'Avi', url:'https://www.linkedin.com/in/avi-gerber/'},
       ]
+    },
+    {
+      src: '/images/ts5.JPG',
+      caption: '📷 by Mone',
+      caption2: '📍 North Dome, Yosemite',
+      captionLink:
+        [
+          { name: "Mone", url: 'https://www.linkedin.com/in/mone-sekiguchi/' },
+          { name: "Tanah Lot, ID", url: 'https://maps.app.goo.gl/2dh6xkvCacoqfcEE9' }
+        ]
     }
   ]
   
@@ -62,7 +72,7 @@ export function getRandomImage(): ImageData {
 
 
 // Export Person interface for use in components
-export type { Person, ImageData }
+export type { Link, ImageData }
 
 // Alternative: if you want consistent randomness per request in SSR
 export function getRandomImageSeeded(seed?: string): string {
